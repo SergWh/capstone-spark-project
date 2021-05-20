2268d2ba56de996b3ab4403cc7bd63192e3c2534
sergey-chernonog <schernonog@gridynamics.com>
2021-05-20 12:29:01 +0300

    fix order and add description

--- src/main/scala/Task2.scala
+++ src/main/scala/Task2.scala
@@ -2,6 +2,10 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class Task2(implicit sparkSession: SparkSession) {
 
+  /**
+   * Task 2.1
+   * Top Campaigns. Plain SQL
+   */
   def getTopCampaigns(df: DataFrame): DataFrame = {
     val viewName = s"campaigns"
     df.createOrReplaceTempView(viewName)
@@ -10,12 +14,16 @@ class Task2(implicit sparkSession: SparkSession) {
       s"FROM $viewName " +
       "WHERE isConfirmed = True " +
       "GROUP BY campaignId " +
-      "ORDER BY billSum " +
+      "ORDER BY billSum DESC " +
       "LIMIT 10"
 
     sparkSession.sql(query)
   }
 
+  /**
+   * Task 2.2
+   * Channels engagement performance. Plain SQL
+   */
   def getMostPopularChannels(df: DataFrame): DataFrame = {
     val viewName = s"channels"
     df.createOrReplaceTempView(viewName)
@@ -23,7 +31,7 @@ class Task2(implicit sparkSession: SparkSession) {
     val query = "SELECT channelId, count(channelId) as sessionsCount " +
       s"FROM $viewName " +
       "GROUP BY channelId " +
-      "ORDER BY sessionsCount "
+      "ORDER BY sessionsCount DESC "
     sparkSession.sql(query)
   }
 
