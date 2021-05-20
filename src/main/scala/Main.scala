49832dbb5a371c170d0e4e8f2cbab20f3a772a02
sergey-chernonog <schernonog@gridynamics.com>
2021-05-20 12:25:42 +0300

    task2 using datasets

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,27 +17,16 @@ object Main {
     val userPurchasesDf = Loader.readCsvGzFiles(userPurchasesFolder)
 
     val task1 = new Task1
-
     val withDf = task1.aggregatePurchasesDf(clickStreamDf)
     val withAggregator = task1.aggregatePurchasesWithCustomAggregator(clickStreamDf)
-
     val purchasesAttributionDf = withDf.join(userPurchasesDf, "purchaseId")
-    purchasesAttributionDf.printSchema()
-    purchasesAttributionDf.explain()
-
     val purchasesAttributionDfWithAggregator = withAggregator.join(userPurchasesDf, "purchaseId")
-    purchasesAttributionDfWithAggregator.printSchema()
-    purchasesAttributionDfWithAggregator.explain()
 
     val task2 = new Task2
-
     val topCampaigns = task2.getTopCampaigns(purchasesAttributionDf)
-    topCampaigns.explain()
-    topCampaigns.printSchema()
-
+    val topCampaignsDataset = task2.getTopCampaignsTyped(purchasesAttributionDf)
     val popularChannels = task2.getMostPopularChannels(purchasesAttributionDf)
-    popularChannels.explain()
-    popularChannels.printSchema()
+    val popularChannelsDataset = task2.getMostPopularChannelsTyped(purchasesAttributionDf)
 
     session.close()
   }
--- src/main/scala/Task2.scala
+++ src/main/scala/Task2.scala
@@ -1,6 +1,12 @@
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import data.{CampaignBillingCost, ChannelCount, Purchase}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.functions.{count, sum, to_timestamp}
+import org.apache.spark.sql.types.DoubleType
+
 
 class Task2(implicit sparkSession: SparkSession) {
+
+  import sparkSession.implicits._
 
   /**
    * Task 2.1
@@ -10,11 +16,11 @@ class Task2(implicit sparkSession: SparkSession) {
     val viewName = s"campaigns"
     df.createOrReplaceTempView(viewName)
 
-    val query = "SELECT campaignId, sum(billingCost) as billSum " +
+    val query = "SELECT campaignId, sum(billingCost) as totalCost " +
       s"FROM $viewName " +
       "WHERE isConfirmed = True " +
       "GROUP BY campaignId " +
-      "ORDER BY billSum DESC " +
+      "ORDER BY totalCost DESC " +
       "LIMIT 10"
 
     sparkSession.sql(query)
@@ -34,5 +40,37 @@ class Task2(implicit sparkSession: SparkSession) {
       "ORDER BY sessionsCount DESC "
     sparkSession.sql(query)
   }
+
+
+  /**
+   * Task 2.1
+   * Top Campaigns. Dataset API
+   */
+  def getTopCampaignsTyped(df: DataFrame): Dataset[CampaignBillingCost] =
+    purchasesDataframeToDataset(df)
+      .groupByKey(_.campaignId)
+      .agg(sum($"billingCost").as[Double].name("totalCost"))
+      .withColumnRenamed("key", "campaignId")
+      .orderBy($"totalCost".desc)
+      .limit(10)
+      .as[CampaignBillingCost]
+
+  /**
+   * Task 2.2
+   * Channels engagement performance. Dataset API
+   */
+  def getMostPopularChannelsTyped(df: DataFrame): Dataset[ChannelCount] =
+    purchasesDataframeToDataset(df)
+      .groupByKey(_.channelId)
+      .agg(count($"channelId").as[Long].name("sessionCount"))
+      .withColumnRenamed("key", "channelId")
+      .orderBy($"sessionCount".desc)
+      .as[ChannelCount]
+
+  private def purchasesDataframeToDataset(df: DataFrame) =
+    df
+      .withColumn("purchaseTime", to_timestamp($"purchaseTime"))
+      .withColumn("billingCost", $"billingCost".cast(DoubleType))
+      .as[Purchase]
 
 }
